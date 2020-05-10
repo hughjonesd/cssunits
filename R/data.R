bd5f99b6062417e4f959488dd71b329b1287e9cd
David Hugh-Jones <davidhughjones@gmail.com>
2020-05-10 21:19:59 +0100

    Move data out of sysdata and back into main package.

--- R/data.R
+++ R/data.R
@@ -73,21 +73,52 @@ css_resolution_db <- data.frame(
 )
 
 
+#' Data frames of CSS and LaTeX units
+#'
+#' The package exports three data frames:  `css_unit_db`,
+#' `latex_unit_db` and `latex_macro_db`.
+#'
+#' The format is not the same as the udunits2 database. Rows are:
+#'
+#' * `symbol`: symbol, e.g. "pt"
+#' * `from_unit`: unit on which the new unit is based, or `NA`
+#' * `new`: is unit new or already defined in udunits2?
+#' * `remove`: list-column of conflicting units to remove from udunits2
+#' * `type`: one of percent, length, angle, duration, frequency, resolution
+#'
+#' Some units are redefined by "cssunits" so as to use a new
+#' symbol. For example, "arc_degree" is present in udunits2, but it is replaced
+#' by a new "deg" unit here.
+#'
+#' @docType data
+#' @author David Hugh-Jones <davidhughjones@gmail.com>
+#' @references
+#' * \url{https://www.w3.org/TR/css3-values/}
+#' * \url{https://raw.githubusercontent.com/debian-tex/texlive-bin/master/texk/web2c/pdftexdir/pdftex.web}
+#' @keywords data
+#' @name css-latex-unit-databases
+NULL
+
+
+#' @export
+#' @rdname css-latex-unit-databases
 css_unit_db <- rbind(css_percent_db, css_length_db, css_angle_db, css_duration_db,
   css_frequency_db, css_resolution_db)
 
 
+#' @export
+#' @rdname css-latex-unit-databases
 latex_unit_db <- data.frame(
-  symbol     = c("pt", "pc", "bp", "dd", "cc", "sp", "nd", "nc", "in", "cm", "mm",
-                 "ex", "em", "mu", "px"),
-  from_unit  = c("in", "pt", "pt", "pt", "pt", "pt", "pt", "pt", NA  , NA,   NA,
-                 NA, NA, NA, "bp"),
-  multiplier = c(72.27, 1/12, 72/72.27, 1157/1238, 1157/14856, 65536, 642/685, 107/1370, NA, NA, NA,
-                 NA, NA, NA, 1),
-  new        = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE,
-                 TRUE, TRUE, TRUE, TRUE),
-  remove     = I(list(c("pt", "t"), none, none, c("d"), c("cc"), none, c("d"), none, none, none, none,
-                 none, none, c("u"), none)),
+  symbol     = c("pt", "pc", "bp", "dd", "cc", "sp", "nd", "nc",
+                 "in", "cm", "mm", "ex", "em", "mu", "px"),
+  from_unit  = c("in", "pt", "pt", "pt", "pt", "pt", "pt", "pt",
+                 NA  , NA,   NA, NA, NA, NA, "bp"),
+  multiplier = c(72.27, 1/12, 72/72.27, 1157/1238, 1157/14856, 65536, 642/685, 107/1370,
+                 NA, NA, NA, NA, NA, NA, 1),
+  new        = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+                 FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE),
+  remove     = I(list(c("pt", "t"), none, none, c("d"), c("cc"), none, c("d"),
+                 none, none, none, none, none, none, c("u"), none)),
   type       = "length",
   stringsAsFactors = FALSE
 )
@@ -111,6 +142,8 @@ latex_length_macros <- c(
 )
 
 
+#' @export
+#' @rdname css-latex-unit-databases
 latex_macro_db <- data.frame(
   symbol     = latex_length_macros,
   from_unit  = NA,
@@ -120,7 +153,3 @@ latex_macro_db <- data.frame(
   type       = "length",
   stringsAsFactors = FALSE
 )
-
-
-usethis::use_data(css_unit_db, latex_unit_db, latex_macro_db,
-      overwrite = TRUE, compress = FALSE, internal = TRUE)
